ad064b8ff72586a060a58f65aba0b8607b2bed2a
Ilia Levchenko <i.levchenko@lampalmpa.net>
2025-07-09 21:36:37 +0300

    feature: Added all ui on the page

---
+++ src/features/index.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { Container } from '@mui/material';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import CloseIcon from '@mui/icons-material/Close';
+import { Heading } from '@components/Heading';
+
+import text from '@shared/texts/index.json';
+import { IconTextButton } from '@components/IconTextButton';
+import { useState } from 'react';
+import { Typography } from '@components/Typography';
+import { Button } from '@components/Button';
+
+const BUTTONS = [
+  {
+    icon: <ContentPasteIcon />,
+    label: text.pasteText,
+    onClick: () => console.log('Paste text'),
+  },
+  {
+    icon: <TextSnippetIcon />,
+    label: text.sampleText,
+    onClick: () => console.log('Sample text'),
+  },
+];
+
+export const RootPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState<{ msg: string } | null>(null);
+  const isInputEmpty = inputValue.length === 0;
+
+  const onInput = (e: React.FormEvent<HTMLTextAreaElement>) =>
+    setInputValue(e.currentTarget.value);
+
+  const clearInputValue = () => setInputValue('');
+  return (
+    <section>
+      <Container className="py-[32px] flex flex-col gap-[40px]">
+        <Heading title={text.title} description={text.description} />
+        <div className="w-full border-[#DBDCDF] border-[1px] rounded-[28px] overflow-hidden relative">
+          {isInputEmpty && (
+            <ul className="absolute z-10 grid grid-cols-2 gap-[16px] top-[50%] translate-[-50%] left-[50%]">
+              {BUTTONS.map(item => (
+                <li key={item.label}>
+                  <IconTextButton {...item} />
+                </li>
+              ))}
+            </ul>
+          )}
+          <textarea
+            value={inputValue}
+            onInput={onInput}
+            name="text-field"
+            className="w-full h-[336px] bg-[#EEF0F5] p-[16px] resize-none  outline-none"
+            placeholder={text.inputPlaseholder}
+          />
+
+          <div className="w-full bg-white h-[64px] p-[8px] flex justify-end gap-[8px]">
+            {!isInputEmpty && (
+              <Button
+                type="button"
+                variant="outlined"
+                className="w-fit flex justify-center items-center gap-[8px] px-[12px]"
+                disabled={inputValue.length === 0}
+                onClick={clearInputValue}
+              >
+                <CloseIcon className="w-[13px] h-[13px]" />
+                <Typography
+                  as="span"
+                  className="text-[14px] text-center"
+                  variant={600}
+                >
+                  {text.clearInput}
+                </Typography>
+              </Button>
+            )}
+
+            <Button
+              type="button"
+              variant="filled"
+              disabled={inputValue.length === 0}
+            >
+              <Typography as="span" className="text-[14px]" variant={600}>
+                {text.paraphrase.start}
+              </Typography>
+            </Button>
+          </div>
+
+          {error && (
+            <div className="text-red">
+              <Typography>{error.msg}</Typography>
+            </div>
+          )}
+        </div>
+      </Container>
+    </section>
+  );
+};
